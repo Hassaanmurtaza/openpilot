a910ed1dcfbc52309f17b9c1e0e207dd4da6adc2
Hassaanmurtaza <metooout@hotmail.com>
2025-08-05 23:53:55 -0400

    comminting initial changes

--- selfdrive/monitoring/test_monitoring.py
+++ selfdrive/monitoring/test_monitoring.py
@@ -7,7 +7,7 @@ from openpilot.selfdrive.monitoring.helpers import DriverMonitoring, DRIVER_MONI
 EventName = car.CarEvent.EventName
 dm_settings = DRIVER_MONITOR_SETTINGS()
 
-TEST_TIMESPAN = 120  # seconds
+TEST_TIMESPAN = int(dm_settings._DISTRACTED_TIME * 3 + 10)  # seconds
 DISTRACTED_SECONDS_TO_ORANGE = dm_settings._DISTRACTED_TIME - dm_settings._DISTRACTED_PROMPT_TIME_TILL_TERMINAL + 1
 DISTRACTED_SECONDS_TO_RED = dm_settings._DISTRACTED_TIME + 1
 INVISIBLE_SECONDS_TO_ORANGE = dm_settings._AWARENESS_TIME - dm_settings._AWARENESS_PROMPT_TIME_TILL_TERMINAL + 1
